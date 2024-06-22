d7e2d5f600f84f1252a282ff8e7136d4c7781161
lizaremizova <er115@inbox.lv>
2024-06-22 13:23:56 +0300

    1 version

--- logic.py
+++ logic.py
@@ -72,14 +72,53 @@ class DatabaseManager:
 
 
     def get_users(self):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('SELECT * FROM users')
         return [x[0] for x in cur.fetchall()] 
         
     def get_prize_img(self, prize_id):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('SELECT image FROM prizes WHERE prize_id = ?', (prize_id,))
         return cur.fetchall()[0][0]
 
     def get_random_prize(self):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('SELECT * FROM prizes WHERE used = 0 ORDER BY RANDOM()')
         return cur.fetchall()[0]
     
+ ##
+
+def get_winners_count(self, prize_id):
+    conn = sqlite3.connect(self.database)
+    with conn:
+        cur = conn.cursor()
+        cur.execute('SELECT COUNT(*) FROM winners WHERE prize_id = ?', (prize_id, ))
+        return cur.fetchall()[0][0]
+   
+   
+    
+def get_rating(self):
+    conn = sqlite3.connect(self.database)
+    with conn:
+        cur = conn.cursor()
+        cur.execute('''
+SELECT users.user-name,
+COUNT(winners.prize_id) as count_prize
+FROM winners
+INNER JOIN users on users.user_id = winners.user_id
+GROUP BY winners.user_id
+ORDER BY count_prize            
+LIMIT 10
+''')
+        return cur.fetchall()
+
+##
   
 def hide_img(img_name):
     image = cv2.imread(f'img/{img_name}')
